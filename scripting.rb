a127c6118adc63ed49526b1c36b55fee6fbbe757
Jeremy Karmel <jkarmel@me.com>
2012-01-01 23:31:38 -0800

    added a method to install DMG files

--- scripting.rb
+++ scripting.rb
@@ -1,3 +1,6 @@
+require "fileutils"
+require "shellwords"
+
 module Scripting
   # Current User's home directory
   def self.Home
@@ -22,5 +25,23 @@ module Scripting
 
   def self.headline(title)
     puts "\n\n**** #{title} ****\n"
+  end
+
+  def self.install_dmg dmg_file
+    self.headline 'installing dmg at location ' + dmg_file
+    # Command mounts volume and result a list of directories, the last
+    # of which is the mounted volume
+    mounted_volume = %x[hdiutil mount #{Shellwords.escape dmg_file}].split("\t")[-1][0..-2]
+    puts "mounted volume #{mounted_volume}"
+    Dir.new(mounted_volume).each do |relative_path|
+      if relative_path.end_with? ".app" 
+        FileUtils.cp_r mounted_volume + '/' +  relative_path, "/Applications"
+        puts "installed application #{relative_path} to Applications directory"
+      end 
+    end
+    Kernel.fork do 
+      sleep 60 * 2 # Wait 2 mins to unmount
+      %x[hdiutil unmount -force #{Shellwords.escape mounted_volume}]
+    end
   end
 end
